7cf952dec0fa8d012f7a35f259c0a75cdd011ea3
Anjie Wang <anjiewg@gmail.com>
2020-10-23 04:13:50 +0000

    added buttons and some css

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ import json
 from youtube import YoutubeVideoData
 from datetime import date
 
+
 app = Flask(__name__)
 
 API_KEY = "AIzaSyB7gBd3yJ6to16PESYfMIcgbf4eP2l60OI"
@@ -13,20 +14,20 @@ def index():
     return render_template("main.html")
 
 
-@app.route('/api/search')
+@app.route('/api/search', methods=["POST"])
 def youtube_video_search():
-    query = request.args.get("keywords")
+    query = request.form.get("keywords") #TODO: create a list to split keywords and loop through; can also jsonify the string 
 
-    published_after = request.args.get("published_after") + "T00:00:00-08:00"
+    published_after = request.form.get("published_after") + "T00:00:00-08:00"
     if published_after == "T00:00:00-08:00":
         published_after = "2000-01-01T00:00:00-08:00"
 
-    published_before = request.args.get("published_before") + "T00:00:00-08:00"
+    published_before = request.form.get("published_before") + "T00:00:00-08:00"
     if published_before == "T00:00:00-08:00":
         published_before = str(date.today()) + "T00:00:00-08:00"
     print(published_before)
     
-    order = request.args.get("order")
+    order = request.form.get("order")
     if order == "Relevancy":
         order = "relevance"
     elif order == "Most Recently Published":
@@ -40,8 +41,6 @@ def youtube_video_search():
     # if max_results == "":
     #     max_results = 25
 
-
-    
     kw_search = YoutubeVideoData(API_KEY, query, order, published_after, published_before)
     channel_data = kw_search.get_youtube_data()
     
--- youtube.py
+++ youtube.py
@@ -3,13 +3,16 @@ import json
 
 class YoutubeVideoData:
 
+
     def __init__(self, api_key, query, order, published_after, published_before):
         self.api_key = api_key
         self.query = query
         self.order = order
-        self.video_ids = None
+        self.channel_ids = set()
         self.published_after = published_after
         self.published_before = published_before
+        self.previous_page_token = None
+        self.next_page_token = None  #TODO: how to persist the next_page_token
 
     
     def get_youtube_data(self):
@@ -23,39 +26,50 @@ class YoutubeVideoData:
             "key" : self.api_key,
             "order" : self.order,
             "publishedBefore" : self.published_before,
-            "publishedAfter" : self.published_after
+            "publishedAfter" : self.published_after,
+            "maxResults" : 50,
+            "relevanceLanguage" : "EN"
         }
 
         request = requests.get(search_url, params=search_params)
         data = json.loads(request.text)
-        channel_ids = []
+
+        self.next_page_token = data['nextPageToken']
+        print(self.next_page_token)
+
+        self.channel_ids = set() #TODO: trying to figure out how to get only unique channel_ids to add to the table
+
         for item in data["items"]:
-            channel_ids.append(item["snippet"]["channelId"])
+            self.channel_ids.add(item["snippet"]["channelId"])
+        
+        print(self.channel_ids)
 
 
         channel_params = {
             "part" : "snippet,statistics,contentOwnerDetails,topicDetails",
             "key" : self.api_key,
-            "id" : ",".join(channel_ids)
+            "id" : ",".join(self.channel_ids),
+            "maxResults" : 50
         }
 
         request = requests.get(channel_url, params=channel_params)
         channel_data = json.loads(request.text)
     
         channel_list = []    
+        
         for item in channel_data["items"]:
             channel_dict = {
             "id" : item["id"],
             "title" : item["snippet"]["title"],
             "description" : item["snippet"]["description"],
             "view_count" : item["statistics"]["viewCount"],
-            "subscriber_count" : item["statistics"]["subscriberCount"],
+            "subscriber_count" : item["statistics"]["subscriberCount"], #TODO: how to handle if no subscribers are returned
             "video_count" : item["statistics"]["videoCount"],
             "published_date" : item["snippet"]["publishedAt"],
-            "url" : "youtube.com/channel/" + item["id"]
+            "url" : "https://www.youtube.com/channel/" + item["id"]
             }
             channel_list.append(channel_dict)
 
 
-        return channel_list
+        return (channel_list)
 
